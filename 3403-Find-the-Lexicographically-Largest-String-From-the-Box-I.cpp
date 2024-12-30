eeb01bb6bb4b89a8295125157f63bf19e84cf894
bharathsans <103253113+bharathsans@users.noreply.github.com>
2024-12-30 13:18:38 +0530

    Update 3403-Find-the-Lexicographically-Largest-String-From-the-Box-I.cpp
    Fix

--- 3403-Find-the-Lexicographically-Largest-String-From-the-Box-I.cpp
+++ 3403-Find-the-Lexicographically-Largest-String-From-the-Box-I.cpp
@@ -1,46 +1,17 @@
 class Solution {
 public:
-
-    bool isPossible(vector<int>& count){
-        int same = -1;
-        for(int i = 0; i < 26; i++){
-            if(count[i] != 0)
-            {
-                if(same == -1)
-                    same = count[i];
-                else
-                    if(count[i] != same)
-                        return false;
-            }
-        }
-        return true;
-    }
-
-    int findMin(string s, int start, vector<int>& dp){
-
-        if(start == s.size())
-            return 0;
+    string answerString(string word, int numFriends) {
+        if(numFriends == 1)
+            return word;
         
-        if(dp[start] != -1)
-            return dp[start];
-
-        int ans = INT_MAX - 2;
-        vector<int> count(26, 0);
-      
-        for(int i = start; i < s.size(); i++){
-            count[s[i] - 'a']++;
-            if(isPossible(count))
-                ans = min(ans, findMin(s, i + 1, dp) + 1);
-        }
+        int maxLen = word.size() - numFriends + 1;
+        string ans = "";
         
-        return dp[start] = ans;
-    }
+        // Substr when it goes out of bounds
+        // Just takes whatever remains...
+        for(int i = 0; i < word.size(); i++)
+            ans = max(ans, word.substr(i, maxLen));
 
-
-    int minimumSubstringsInPartition(string s) {
-        int n = s.size();
-        vector<int> dp(n, -1);
-
-        return findMin(s, 0, dp);
+        return ans;
     }
 };
